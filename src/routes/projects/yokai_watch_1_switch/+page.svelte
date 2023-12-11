407c19d416ea087a96ab9ba3bdaf70a4f1c64d0e
PokeFred <cedric.paelmke@gmx.de>
2023-12-11 21:12:56 +0100

    Change Project Pages

---
+++ src/routes/projects/yokai_watch_1_switch/+page.svelte
@@ -0,0 +1,4 @@
+<div class="mx-16 w-auto h-auto text-left">
+    <div class="text-3xl text-center underline pb-2">Yokai Watch 1 (Switch)</div>
+    <div class="text-lg">lorem ipsum</div>
+</div>
---
+++ src/routes/projects/yokai_watch_4/+page.svelte
@@ -0,0 +1,4 @@
+<div class="mx-16 w-auto h-auto text-left">
+    <div class="text-3xl text-center underline pb-2">Yokai Watch 4++</div>
+    <div class="text-lg">lorem ipsum</div>
+</div>
