e24b18737a7873f5e4f954bb208309d617151b99
PokeFred <cedric.paelmke@gmx.de>
2023-12-11 20:10:59 +0100

    add rebuild on restart on plesk

--- entry.cjs
+++ entry.cjs
@@ -1,5 +1,23 @@
+const child_process = require("child_process")
+
+function buildApp() {
+    const args = process.argv.slice(2)
+
+    let status = true
+    args.forEach((element) => {
+        if (element === "--rebuild=true") {
+            status = true
+        }
+    })
+
+    if (status) {
+        child_process.execSync("npm run build")
+    }
+}
+
 async function loadApp() {
     const { app } = await import("./app.js")
 }
 
+buildApp()
 loadApp()
