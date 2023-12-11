b742a048598eb10e99016779133ac4b07811c7fd
PokeFred <cedric.paelmke@gmx.de>
2023-12-11 21:50:19 +0100

    Update entry.cjs

--- entry.cjs
+++ entry.cjs
@@ -1,14 +1,5 @@
-const {execSync} = require("child_process")
-
-function rebuildApp() {
-    if (process.env.REBUILD === "true") {
-        execSync("npm install && npm run build")
-    }
-}
-
 async function loadApp() {
     const { app } = await import("./app.js")
 }
 
-rebuildApp()
 loadApp()
