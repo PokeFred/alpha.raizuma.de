a94029b1c5d9ae52ad0501ce80ac0c86dfdbf0fb
PokeFred <Cedric.paelmke@gmx.de>
2023-12-13 11:37:27 +0100

    add 8-pit to team

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -2,15 +2,14 @@
     import ProfileCard from "$components/ProfileCard.svelte"
 </script>
 <div class="sm:mx-8 md:mx-16 mb-4 w-auto h-auto text-2xl lg:text-4xl text-center bg-slate-800/90 rounded-xl">Das Team</div>
-<div>
-    <div class="h-auto flex flex-wrap justify-center items-center">
-        <ProfileCard name="Hachiro" position="CEO" image="/team/hachiro.png" />
-        <ProfileCard name="Vidpic" position="Leitung Yokai Watch Deutschland" image="/team/vidpic.png" />
-        <ProfileCard name="Sprössling" position="Leitung Yokai Watch Deutschland" image="/team/sprössling.png" />
-        <ProfileCard name="Marv" position="Leitung Yokai Watch & Deltarune Deutschland" image="/team/marv.png" />
-        <ProfileCard name="Nagisa Shiota" position="Leitung Deltarune Deutschland & Developer" image="/team/sev.jpeg" />
-        <ProfileCard name="PokeFred" position="Developer" image="/team/pokefred.png" />
-        <ProfileCard name="Ericsucht" position="Direct Leitung" image="/team/ericsucht.png" />
-        <ProfileCard name="Etendo" position="Direct Leitung" image="/team/etendo.png" />
-    </div>
+<div class="h-auto flex flex-wrap justify-center items-center">
+    <ProfileCard name="Hachiro" position="CEO" image="/team/hachiro.png" />
+    <ProfileCard name="8-Pit" position="Mitgründer von Yokai Watch Deutschland" image="/team/8-pit.png" />
+    <ProfileCard name="Vidpic" position="Leitung Yokai Watch Deutschland" image="/team/vidpic.png" />
+    <ProfileCard name="Sprössling" position="Leitung Yokai Watch Deutschland" image="/team/sprössling.png" />
+    <ProfileCard name="Marv" position="Leitung Yokai Watch & Deltarune Deutschland" image="/team/marv.png" />
+    <ProfileCard name="Nagisa Shiota" position="Leitung Deltarune Deutschland & Developer" image="/team/sev.jpeg" />
+    <ProfileCard name="PokeFred" position="Developer" image="/team/pokefred.png" />
+    <ProfileCard name="Ericsucht" position="Direct Leitung" image="/team/ericsucht.png" />
+    <ProfileCard name="Etendo" position="Direct Leitung" image="/team/etendo.png" />
 </div>
