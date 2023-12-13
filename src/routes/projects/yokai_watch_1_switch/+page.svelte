370e59fac21e023a190617737c6c2e93d91fca59
PokeFred <Cedric.paelmke@gmx.de>
2023-12-13 12:58:48 +0100

    change project text layouts

--- src/routes/projects/yokai_watch_1_switch/+page.svelte
+++ src/routes/projects/yokai_watch_1_switch/+page.svelte
@@ -5,5 +5,14 @@
             <img src="/projects_data/yokai-watch-1-icon.png" class="w-auto h-auto max-h-64 aspect-square rounded-xl" alt="">
         </div>
     </div>
-    <div class="mt-4">lorem ipsum</div>
+    <div class="mt-4">
+        Entwickler: Level-5<br>
+        Publisher: Level-5 und Nintendo<br><br>
+
+        Veröffentlicht: 10 Oktober 2019 (Switch)<br>
+        Originalsprache: Japanisch<br>
+        Altersfreigabe: USK 6<br><br>
+
+        Im Spiel Yo-Kai Watch für die Switch spielt man den Charakter Nathan oder Katie basierend auf die Entscheidung des Spielers und erlebt die gesamte Geschichte wie in einem Rollenspiel. Auf der Reise trifft man verschiedene Charaktere, die entweder ein Mensch oder ein Yokai (Geisterähnliche Wesen aus der japanischen Mythologie) sind. Währendessen kann man sich mit einigen der Yokais anfreunden, mit diesen gegen anderen kämpfen, aufleven und vieles mehr! Erlebe somit die gesamte Geschichte von Yo-Kai Watch nun auf der inoffizielen deutschen Version von Raizuma!
+    </div>
 </div>
--- src/routes/projects/yokai_watch_4/+page.svelte
+++ src/routes/projects/yokai_watch_4/+page.svelte
@@ -5,5 +5,14 @@
             <img src="/projects_data/yokai-watch-4-icon.png" class="w-auto h-auto max-h-64 aspect-square rounded-xl" alt="">
         </div>
     </div>
-    <div class="mt-4">lorem ipsum</div>
+    <div class="mt-4">
+        Entwickler: Level-5<br>
+        Publisher: Level-5<br><br>
+
+        Veröffentlicht: 20 Juni 2019 (Switch)<br>
+        Originalsprache: Japanisch<br>
+        Altersfreigabe: USK 6<br><br>
+
+        Das Spiel Yo-Kai Watch 4++ für die Switch ähnelt dem Konzept von Yo-Kai Watch 1. Man kann verschiedene Yokais antreffen, diese befreunden und mit diesen Kämpfen. Die Geschichte spielt in 3 verschiedenen Zeitlinien, die das Konzept von Vergangenheit, Gegenwart und Zukunft vermitteln. Des weiteren erlaubt das Kampfsystem des Spiels frei die Yokais und den Mensch zu steuern und mit denen, wie in enem Action-RP Spiel, zu kämpfen. Hier hat Raizuma viel Arbeit investiert, um eine gute inoffizielle deutsche Version des Spiels zu ermöglichen! Viel Spaß damit und seid auf weiteren Updates gespannt!
+    </div>
 </div>
