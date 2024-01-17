db7efb8fce5e2450329668f58ebb331d25ac1ac1
PokeFred <cedric.paelmke@gmx.de>
2024-01-17 22:22:13 +0100

    small update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,30 @@
-<div class="mx-8 mb-4 flex h-auto w-auto items-center justify-center rounded-xl bg-slate-800/90 text-2xl md:mx-16 lg:text-4xl">Über uns</div>
+<script lang="ts">
+    import { onMount } from 'svelte'
 
+    let content: string = ''
+    onMount(async () => {
+        try {
+            const response: Response = await fetch('/api/about', {
+                method: 'GET',
+            })
+
+            if (response.status === 200) {
+                const data: string = await response.text()
+                content = data
+            } else {
+                content = 'Error'
+            }
+        } catch (error: unknown) {
+            content = 'Error'
+        }
+    })
+</script>
+
+<div class="mx-8 mb-4 flex h-auto w-auto items-center justify-center rounded-xl bg-slate-800/90 text-2xl md:mx-16 lg:text-4xl">Über uns</div>
+<div class="text-1 mx-8 md:mx-16 lg:text-2xl">{@html content}</div>
+
+<!--
+<div class="mx-8 mb-4 flex h-auto w-auto items-center justify-center rounded-xl bg-slate-800/90 text-2xl md:mx-16 lg:text-4xl">Über uns</div>
 <div class="text-1 mx-8 md:mx-16 lg:text-2xl">
     Unser Ziel bei Raizuma ist es, mit unseren Übersetzungen verschiedenste Videospiele für deutsche Spieler zugänglicher zu machen. Wir nehmen uns die Zeit für Optimierungen jeder Zeile und jedes Dialogfeldes, um den Charme und die Atmosphäre des
     Originalspiels beizubehalten.<br /><br />
@@ -10,3 +35,4 @@
     Vielen Dank, dass Ihr uns auf dieser aufregenden Reise begleitet. Wir freuen uns, unsere Leidenschaft für Spiele wie Yo-kai Watch und Deltarune sowie andere Spiele mit Euch zu teilen und zu sehen, dass Ihr mit den Übersetzungen Spaß habt. Eine
     Fremdsprache sollte niemals ein Hindernis für eine angenehme Spielerfahrung sein und wir stellen sicher, dass Eure Spielerfahrung so angenehm wie möglich ist.
 </div>
+-->
